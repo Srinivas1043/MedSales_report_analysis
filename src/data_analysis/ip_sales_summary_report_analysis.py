210d7e74d60f3bba202ef145116c8f0b10ddea41
RamyaRagu2506 <ramyaanu2003@gmail.com>
2023-03-19 20:52:21 +0530

    streamlit_for_all_files_done

--- src/data_analysis/ip_sales_summary_report_analysis.py
+++ src/data_analysis/ip_sales_summary_report_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import streamlit as st
 import plotly.express as px
-df = pd.read_excel('../data/final/Ip_Sales_Summary_Report_Final.xlsx')
+df = pd.read_excel('../../data/final/Ip_Sales_Summary_Report_Final.xlsx')
 st.title('SIMS Sales Summary Dashboard')
 st.subheader('Dataset')
 st.dataframe(df)
