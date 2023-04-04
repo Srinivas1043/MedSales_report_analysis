7444a978422bf554f4ba2be7b90fcc8b63b66a32
RamyaRagu2506 <102846337+RamyaRagu2506@users.noreply.github.com>
2023-04-04 05:40:23 +0200

    Modified app.py
    Added comments, docstrings to explain the function and print statements for each graph.

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, dash_table
 import plotly.express as px
+from dash_bootstrap_templates import ThemeSwitchAIO
 
 
 # define app and server
@@ -52,6 +53,16 @@ app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
     [dash.dependencies.State("sidebar", "style"), dash.dependencies.State("page-content", "style")],
 )
 def toggle_sidebar(n_clicks, sidebar_style, content_style):
+    """Toggle the visibility of a sidebar and adjust the margins of the content accordingly.
+
+    Parameters:
+        n_clicks (int or None): The number of times the toggle button has been clicked.
+        sidebar_style (dict): A dictionary of CSS styles for the sidebar element, including its current margin-left value.
+        content_style (dict): A dictionary of CSS styles for the content element, including its current margin-left value.
+
+    Returns:
+        tuple: A tuple of two dictionaries containing the updated CSS styles for the sidebar and content elements, respectively.
+    """
     if n_clicks is None:
         return sidebar_style, content_style
     if sidebar_style.get("margin-left") == "0px":
@@ -67,21 +78,45 @@ ip_discount_report_df =pd.read_excel('data/final/Ip_Discount_Report_Final.xlsx')
 
 
 def ip_discount_pie_chart():
+    """ This function returns a pie chart 
+    visualization using Plotly Express 
+    (px.pie()) of the total amount of 
+    discount provided by each insurance name in 
+    the ip_discount_report_df dataframe.    """
     return px.pie(ip_discount_report_df,values='TOTAL AMOUNT',names = 'INSURANCE NAME')
 
 def ip_discount_bar():
+    """This function returns a bar chart visualization using Plotly Express (px.bar()) of the 
+    discount provided by each insurance name in the ip_discount_report_df dataframe."""
+    
     return px.bar(ip_discount_report_df,x='INSURANCE NAME',y='DISCOUNT')
 
 def ip_scatter_total_discount():
+    """This function returns a scatter plot visualization using 
+    Plotly Express (px.scatter()) of the relationship between 
+    the total amount and discount provided by each insurance 
+    name in the ip_discount_report_df dataframe, 
+    with each insurance name represented by a different color.    """
+    
     return px.scatter(ip_discount_report_df,x='TOTAL AMOUNT',y='DISCOUNT',color = 'INSURANCE NAME')
+
 @callback(
     Output('discount-graph-content', 'figure'),
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
+    """
+    Update the line chart based on the selected insurance name value.
+
+    Args:
+        value (str): The selected value from the insurance name dropdown.
+
+    Returns:
+        plotly.graph_objs._figure.Figure: A line chart showing the discount amounts
+        for the selected insurance name over time.
+    """
     dff = ip_discount_report_df[ip_discount_report_df['INSURANCE NAME']==value]
     return px.line(dff, x='BILL DATE', y='DISCOUNT')
-
 
 
 ip_discount_report = html.Div([
@@ -118,26 +153,100 @@ equipment_ip_sales_detail = html.Div([
 # define page content for IP Sales Details sheet
 ip_sales_details_df = pd.read_excel('data/final/Ip_Sales_Details_Final.xlsx')
 def admit_discharge_date_bill_amount_analysis():
+    """
+    Creates a scatter plot of bill amount versus number of days of hospitalization, colored by company and with specialization shown on hover.
+    The function calculates the number of days of hospitalization for each record in the ip_sales_details_df DataFrame by subtracting the admitdatetime from the dischargedatetime columns, and then converts the resulting timedelta object to days using pd.to_timedelta(). The resulting number of days is then plotted on the x-axis, and the bill amount on the y-axis. Each point is colored based on the value in the Company column, and hovering over a point shows the corresponding value in the Specialisation column. The resulting plot is returned as a Plotly Express scatter plot.
+    Returns:
+    A Plotly Express scatter plot of bill amount versus number of days of hospitalization, colored by company and with specialization shown on hover.
+
+    """
+    
     ip_sales_details_df['duration'] = ip_sales_details_df['dischargedatetime'] -ip_sales_details_df['admitdatetime']
     ip_sales_details_df['numberofdays'] = ip_sales_details_df['duration'].apply(pd.to_timedelta).dt.days
     return px.scatter(ip_sales_details_df, x='numberofdays', y='billamount', color="Company", hover_name="Specialisation", size='numberofdays')
 
 def ip_netamt_specialisation_bar():
+    """
+    Creates a horizontal bar chart using Plotly Express to visualize the net amount billed for each medical specialization in the ip_sales_details_df DataFrame, with the bars colored by category.
+
+    Returns:
+    --------
+    A Plotly graph object of a horizontal bar chart showing the net amount billed for each medical specialization in the DataFrame.
+    """
     return px.bar(ip_sales_details_df,y= 'Specialisation',x='netamount',color ='category')
 
 def ip_netamt_cat_bar():
+    """
+    This function creates a bar chart using the Plotly Express library to display the net amount of sales for each category in the 'ip_sales_details_df' dataset. The y-axis represents the categories, the x-axis represents the net amount of sales, and the color of the bars represents the specialisation of the sale.
+
+    Returns:
+    --------
+    fig : plotly.graph_objs._figure.Figure
+        A Plotly figure object representing the bar chart.
+    """
     return px.bar(ip_sales_details_df,y= 'category',x='netamount',color ='Specialisation')
 
 def ip_net_amt_company_bar():
+    """
+    Returns a bar chart using Plotly Express that shows the net amount of each company, 
+    colored by area.
+
+    Parameters:
+    None.
+
+    Returns:
+    fig (plotly.graph_objs._figure.Figure): Plotly bar chart object.
+    """
     return px.bar(ip_sales_details_df,y = 'Company',x='netamount',color ='Area')
 
 def ip_package_fees_specialisation_bar():
+    """
+Creates a bar chart using Plotly Express to show the package fees for each specialisation of the IP sales details data, 
+colored by the category.
+
+Returns:
+- A Plotly Express bar chart object.
+
+Input:
+- ip_sales_details_df: A Pandas DataFrame containing IP sales details data with columns 'Specialisation', 'PackageFees',
+  and 'category'.
+  
+Output:
+- A Plotly Express bar chart object with 'Specialisation' on the y-axis, 'PackageFees' on the x-axis, and colored by 'category'.
+"""
     return px.bar(ip_sales_details_df,y='Specialisation',x='PackageFees',color ='category')
 
 def ip_package_fees_cat_bar():
+    """
+    Returns a Plotly bar 
+    chart showing the distribution of package fees 
+    for each category of medical specialization in the inpatient sales details dataset.
+
+    Args:
+        None
+    
+    Returns:
+     A Plotly bar chart object displaying the package fees for each category of medical specialization.
+    
+    Raises:
+        None
+
+    """
     return px.bar(ip_sales_details_df,y='category',x='PackageFees',color ='Specialisation')
 
 def ip_package_fees_company_bar():
+    """
+Creates a bar chart using the Plotly Express library to show the relationship between the package fees charged by companies for inpatient services and the area in which they are located.
+
+Args:
+    None
+
+Returns:
+    A Plotly bar chart object representing the relationship between the package fees charged by companies for inpatient services and the area in which they are located.
+
+Raises:
+    None
+    """
     return px.bar(ip_sales_details_df,y ='Company',x='PackageFees',color ='Area')
 
 
@@ -145,61 +254,152 @@ ip_sales_details = html.Div([
     html.H1("IP Sales Details"),
     html.Br(),
     # insert code for IP Sales Details visualization here
+    html.H4('Bill amount vs Duration'),
+    html.P('The visualization shows the duration (the difference between admit date and dischard date) \
+           of a patient being admitted to the hospital and the related bill amount for the same.'),
     dcc.Graph(id='discharge-difference', figure=admit_discharge_date_bill_amount_analysis()),
+ 
+    html.H4('Net Amount vs Specialisation'),
+    html.P('The visualization shows the net amount of the hospital being raised based on the Specialization. The filter is used related to the Comopany.'),
     dcc.Graph(id='ip_netamount_specialisation', figure=ip_netamt_specialisation_bar()),
+ 
+    html.H4('Net Amount vs Category'),
+    html.P('The visualization shows the net amount of the hospital being raised based on the Category. The filter is used related to the Specialization.'),
     dcc.Graph(id='ip_netamount_category', figure=ip_netamt_cat_bar()),
+
+    html.H4('Net Amount vs Company'),
+    html.P('The visualization shows the net amount of the hospital being raised based on the Company. The filter is used related to the Area/State.'),
     dcc.Graph(id='ip_netamount_company', figure=ip_net_amt_company_bar()),
+    
+    html.H4('Package Fees vs Specialisation'),
+    html.P('The visualization shows the Package Fees of the hospital being raised based on the Specialization. The filter is used related to the category.'),
     dcc.Graph(id='ip_packagefees_specialisation', figure=ip_package_fees_specialisation_bar()),
+    
+    html.H4('Package Fees vs category'),
+    html.P('The visualization shows the Package Fees of the hospital being raised based on the category. The filter is used related to the Specialization.'),
     dcc.Graph(id='ip_packagefees_category', figure=ip_package_fees_cat_bar()),
+    
+    html.H4('Package Fees vs Company'),
+    html.P('The visualization shows the Package Fees of the hospital being raised based on the Company. The filter is used related to the Area/State.'),
     dcc.Graph(id='ip_packagefees_company', figure=ip_package_fees_company_bar()),
 ])
 
 
+
 # define page content for IP Sales Report sheet
 ip_sales_report_df = pd.read_excel('data/final/Ip_Sales_Report_Final.xlsx')
-def ip_bill_amount_sales_report_company_bar():
-    return px.line(ip_sales_report_df,y ='BILL AMOUNT',x='datetime',color ='COMPANY')
-def ip_discount_sales_report_company_bar():
-    return px.line(ip_sales_report_df,y ='DISCOUNT',x='datetime',color ='COMPANY')
-def ip__sales_report_company_pie():
-    return px.pie(ip_sales_report_df,values ='BILL AMOUNT',names ='COMPANY')
 
+
+def ip_bill_amount_sales_report_company_bar():
+    """
+Creates a line chart using Plotly Express that shows the trend of the total bill amount for each company in the given IP sales report dataframe over time.
+
+Parameters:
+ip_sales_report_df (pandas.DataFrame): the IP sales report dataframe containing the following columns - 'datetime', 'COMPANY', 'BILL AMOUNT'
+
+Returns:
+A Plotly Express line chart displaying the trend of the total bill amount for each company in the given IP sales report dataframe over time.
+"""
+
+    return px.line(ip_sales_report_df,y ='BILL AMOUNT',x='datetime',color ='COMPANY')
+
+def ip_discount_sales_report_company_bar():
+    """
+Returns an area plot of the discount amounts over time for each company in the IP sales report DataFrame.
+
+Parameters:
+None
+
+Returns:
+A Plotly express area plot of the discount amounts over time for each company in the IP sales report DataFrame.
+"""
+
+    return px.area(ip_sales_report_df,y ='DISCOUNT',x='datetime',color ='COMPANY')
 
 ip_sales_report = html.Div([
     html.H1("IP Sales Report"),
     html.Br(),
     # insert code for IP Sales Report visualization here 
-dcc.Graph(id='ip_bill_amount_sales_report_company_bar', figure=ip_bill_amount_sales_report_company_bar()),
-dcc.Graph(id='ip_discount_sales_report_company_bar', figure=ip_discount_sales_report_company_bar()),
-dcc.Graph(id='ip_sales_report_company_pie', figure=ip__sales_report_company_pie())
+    html.H4('Bill Amount vs Date'),
+    html.P('The visualization shows the Bill amount of the hospital being raised based on the date time. The filter is used related to the Company. It is a time series analysis.'),
+    dcc.Graph(id='ip_bill_amount_sales_report_company_bar', figure=ip_bill_amount_sales_report_company_bar()),
+    
+    html.H4('Discount vs Date'),
+    html.P('The visualization shows the Discount of the hospital being raised based on the date time. The filter is used related to the Company. It is a time series analysis.'),
+    dcc.Graph(id='ip_discount_sales_report_company_bar', figure=ip_discount_sales_report_company_bar()),
+
 ])
 
-# define page content for IP Sales Summary Report sheet
 # define page content for IP Sales Summary Report sheet
 ip_sales_summary_df = pd.read_excel('data/final/Ip_Sales_Summary_Report_Final.xlsx')
 
 def admit_discharge_room_rent_analysis():
+    """
+    Generates a scatter plot using Plotly Express that shows the relationship between the length of stay in the hospital 
+    (number of days between admission and discharge) and the room rent amount charged for each patient. The color of each point 
+    corresponds to the patient's diagnosis and the size of the point corresponds to the total bill amount charged to the patient. 
+
+    Returns:
+    --------
+    fig : plotly.graph_objs._figure.Figure
+        A Plotly figure object that can be displayed using `plotly.offline.iplot()` or `plotly.io.show()`.
+    """
     ip_sales_summary_df['duration'] = ip_sales_summary_df['DischargeDatetime'] - ip_sales_summary_df['AdmitDatetime']
     ip_sales_summary_df['numberofdays'] = ip_sales_summary_df['duration'].apply(pd.to_timedelta).dt.days
     return px.scatter(ip_sales_summary_df, x='numberofdays', y='Room Rent', color="Diagnosis", hover_name="Category", size='Bill Amount')
 
 
 def ip_pharm_diagnosis_bar():
+    """Returns a Plotly bar chart showing the pharmacy sales for each diagnosis in the IP sales summary dataframe, with color denoting the state.
+
+Parameters:
+None
+
+Returns:
+A Plotly bar chart object.
+    """
     return px.bar(ip_sales_summary_df,y ='Diagnosis',x='Pharmacy',color ='State')
 
 def ip_docfees_surgerydept_bar():
+    """This function returns a bar chart created using the Plotly Express library. The chart displays the total doctor fees paid for each type of surgery in the given dataset, grouped by country. The y-axis represents the different types of surgery and the x-axis represents the total doctor fees paid for each surgery type.
+    The color of the bars represents the country where the surgery was performed.
+
+    Returns:
+        A Plotly bar chart object.
+    """
     return px.bar(ip_sales_summary_df,y ='SURGERYTYPE',x='Doctor Fees',color ='Country')
 
 def ip_food_beverages_pie_chart():
+    """
+    Generates a pie chart showing the total amount of food and beverages sales for each company, colored by test type.
+
+    Returns:
+    --------
+    A plotly express pie chart object.
+    """
     return px.bar(ip_sales_summary_df,x='Food and Beverages',y = 'Company', color='Test')
 
 ip_sales_summary_report = html.Div([
     html.H1("IP Sales Summary Report"),
     html.Br(),
+   
+    
     # insert code for IP Sales Summary Report visualization here
+    html.H4('Room rent vs Duration'),
+    html.P('The visualization shows the scatter plot of duration of the patient in the hospital and the room rent paid.'),
     dcc.Graph(id='admit_discharge_room_rent_analysis', figure=admit_discharge_room_rent_analysis()),
+    
+    html.H4('Diagnosis vs pharmacy fees'),
+    html.P('The visualization shows the bar plot of pharmacy fees of the hospital and the Diagnosis used.'),
     dcc.Graph(id='ip_pharm_diagnosis_bar', figure=ip_pharm_diagnosis_bar()),
+    
+    
+    html.H4('Surgery Type vs Doctor fees'),
+    html.P('The visualization shows the bar plot of Doctor fees of the hospital and the Surgery Type used.'),
     dcc.Graph(id='ip_docfees_surgerydept_bar', figure=ip_docfees_surgerydept_bar()),
+    
+    html.H4('Company vs Food and Beverages cost'),
+    html.P('The visualization shows the bar plot of Food and Beverages cost of the hospital and the Company.'),
     dcc.Graph(id='ip_food_beverages_pie_chart', figure=ip_food_beverages_pie_chart()),
 ])
 
@@ -215,7 +415,7 @@ cards = dbc.Row([
                 [
                     html.H4("Sales Detail Analysis", className="card-title"),
                     html.P(f"There are {len(ip_sales_details_df)} rows"),
-                    dbc.Button("Go to Page 1", color="primary", href="/ip_sales_details"),
+                    dbc.Button("Sales", color="primary", href="/ip_sales_details"),
                 ]
             ),
             className="mb-3",
@@ -235,7 +435,47 @@ cards = dbc.Row([
                 [
                     html.H4("Discount Report Analysis", className="card-title"),
                     html.P(f"There are {len(ip_discount_report_df)} rows"),
-                    dbc.Button("Go to Page 2", color="primary", href="/ip_discount_report"),
+                    dbc.Button("Discount", color="primary", href="/ip_discount_report"),
+                ]
+            ),
+            className="mb-3",
+            style={
+                'background-image': 'url("https://www.absolutdata.com/wp-content/uploads/2022/06/Is-Dark-Data-the-Key-to-Transforming-Your-Business-blog.jpg")',
+                'background-size': 'cover',
+                'background-position': 'center',
+                'color': 'white'
+            }
+        ),
+        width=6,
+        md={'size': 6, 'offset': 0}
+    ),
+    dbc.Col(
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H4("Sales Report Analysis", className="card-title"),
+                    html.P(f"There are {len(ip_sales_report_df)} rows"),
+                    dbc.Button("Sales Report", color="primary", href="/ip_sales_report"),
+                ]
+            ),
+            className="mb-3",
+            style={
+                'background-image': 'url("https://www.absolutdata.com/wp-content/uploads/2022/06/Is-Dark-Data-the-Key-to-Transforming-Your-Business-blog.jpg")',
+                'background-size': 'cover',
+                'background-position': 'center',
+                'color': 'white'
+            }
+        ),
+        width=6,
+        md={'size': 6, 'offset': 0}
+    ),
+    dbc.Col(
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H4("Sales Summary Report Analysis", className="card-title"),
+                    html.P(f"There are {len(ip_sales_summary_df)} rows"),
+                    dbc.Button("Sales Summary", color="primary", href="/ip_sales_summary_report"),
                 ]
             ),
             className="mb-3",
